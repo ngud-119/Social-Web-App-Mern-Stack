1d0b49f3f5ed0085074fe10a2987e043baad9499
ngud-119 <passiondev7@gmail.com>
2023-04-16 20:19:48 +0530

    added followersCard

---
+++ client/src/Components/Data/FollowersData.js
@@ -0,0 +1,12 @@
+import img1 from '../../Img/imgF1.png';
+import img2 from '../../Img/imgF2.png';
+import img3 from '../../Img/imgF3.jpg';
+import img4 from '../../Img/imgF4.png';
+
+
+export const Followers = [ 
+    {name : "Priya Kapoor", username: "PriyaKapoor", img: img1},
+    {name : "Rahul SK", username: "RSK", img: img2},
+    {name : "Natasha", username: "Natasha", img: img3},
+    {name : "Tom Cruise", username: "TomCruise", img: img4},
+];
--- client/src/Components/profileSide/ProfileSide.js
+++ client/src/Components/profileSide/ProfileSide.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './ProfileSide.css'
 import LogoSearch from '../LogoSearch/LogoSearch'
 import ProfileCard from '../ProfileCard/ProfileCard'
+import FollowersCard from '../FollowersCard/FollowersCard'
 
 const ProfileSide = () => {
   return (
     <div className='ProfileSide'>
          <LogoSearch />
          <ProfileCard />
+         <FollowersCard />
     </div>
   )
 }
