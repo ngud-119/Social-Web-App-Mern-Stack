3f954f5a4650fc0307d0e81a5daa23f42f2fd1cd
ngud-119 <passiondev7@gmail.com>
2023-05-17 13:42:13 +0530

    added react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,25 @@
+import { useSelector } from 'react-redux';
 import './App.css';
 import Auth from './Pages/auth/Auth';
 import Home from './Pages/home/Home';
 import Profile from './Pages/profile/Profile';
-
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
+
+  const user = useSelector((state) => state.authReducer.authData);
+
   return (
     <div className="App">
-      <div className="blur" style={{top:'-18%', right:'0'}}></div>
-      <div className="blur" style={{top:'36%', left:'-8rem'}}></div>
+      <div className="blur" style={{ top: '-18%', right: '0' }}></div>
+      <div className="blur" style={{ top: '36%', left: '-8rem' }}></div>
 
-      {/* <Home /> */}
-      {/* <Profile /> */}
-      <Auth />
+
+      <Routes>
+        <Route path='/' element={user ? <Navigate to='home' /> : <Navigate to='auth' />} />
+        <Route path='/home' element={user ? <Home /> : <Navigate to='../auth' />} />
+        <Route path='/auth' element={user ? <Navigate to='../home' /> : <Auth />} />
+      </Routes>
 
     </div>
   );
--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,19 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './store/ReduxStore.js';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+
+      <BrowserRouter>
+        <Routes>
+          <Route path='*' element= {<App />} />
+        </Routes>
+      </BrowserRouter>
+
     </Provider>
   </React.StrictMode>
 );
