b1f58e6334293af97eee14497552d2e8c627eb64
ngud-119 <passiondev7@gmail.com>
2023-06-12 16:11:38 +0530

    modified timeline Post Section

--- client/src/Components/Posts/Posts.js
+++ client/src/Components/Posts/Posts.js
@@ -3,18 +3,24 @@ import './Posts.css';
 import Post from '../Post/Post';
 import { useDispatch, useSelector } from 'react-redux';
 import { getTimelinePosts } from '../../actions/PostAction';
+import { useParams } from 'react-router-dom';
 
 const Posts = () => {
 
+  const params = useParams();
   const dispatch = useDispatch()
   const { user } = useSelector((state) => state.authReducer.authData)
-  const { posts, loading } = useSelector((state) => state.postReducer)
+  let { posts, loading } = useSelector((state) => state.postReducer)
 
 
   useEffect(() => {
     dispatch(getTimelinePosts(user._id))
   }, [])
 
+
+  if (params.id) {
+    posts = posts.filter((post) => post.userId === params.id)
+  }
 
   return (
     <div className='Posts'>
--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -9,6 +9,16 @@ const postReducer = (
             return { ...state, posts: [action.data, ...state.posts], uploading: false, error: false }
         case "UPLOAD_FAIL":
             return { ...state, uploading: false, error: true }
+
+
+        // belong to Posts.js
+        case "RETRIEVING_START":
+            return { ...state, loading: true, error: false }
+        case "RETRIEVING_SUCCESS":
+            return { ...state, posts: action.data, loading: false, error: false }
+        case "RETRIEVING_FAIL":
+            return { ...state, loading: false, error: true }
+
         default:
             return state
     }
